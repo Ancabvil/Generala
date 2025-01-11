509955758a945739a0b9deaef75f8bd1442ea517
Ancabvil <acabvil562@g.educaand.es>
2025-01-11 19:04:50 +0100

    creación de elementos para usar

--- frontend/generala/src/app/app.routes.ts
+++ frontend/generala/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+];
--- frontend/generala/src/app/navbar/navbar.component.ts
+++ frontend/generala/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router){}
+
+  onLogin(){
+    this.router.navigate(['/login']);
+  }
+
+  onRegister(){
+    this.router.navigate(['/register']);
+  }
 
 }
